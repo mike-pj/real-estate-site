f8779391160f9541040b072f2d4a0a50bc50f7d6
Michael Uzoigwe <michaeluzoigwe78@gmail.com>
2025-12-26 19:28:51 +0100

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { store } from './redux/store.js';
+import { persistor, store } from './redux/store.js';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    {/* from store we come here to wrap so we can access the global state in any component */}
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
 )
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice.js';
+import { persistReducer, persistStore } from 'redux-persist';   
+import storage from 'redux-persist/lib/storage';
+
+
+//persist the redux store in local storage so that when the user refreshes the page, the state is not lost
+const rootReducer = combineReducers({
+  user: userReducer,
+})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+} 
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {user: userReducer},
+  // reducer: {user: userReducer},
+
+  reducer: persistedReducer,//use the persisted reducer instead of the original reducer
 
   //serialize in other to avoid errors with non-serializable values in the state
   middleware: (getDefaultMiddleware) =>
@@ -10,3 +28,6 @@ export const store = configureStore({
       serializableCheck: false,
     }),
 })
+
+//to make the store persisted
+export const persistor = persistStore(store);
